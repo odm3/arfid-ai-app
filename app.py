cef684dad81d9d66048894005d2ded0bc9eb2e93
Oscar D McCullough <1268558+odm3@users.noreply.github.com>
2025-04-13 19:31:37 -0400

    Queue changes

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ def run_openai_task(thread_id, assistant_id):
                                 logger.info(f"Assistant role message: {msg.role}")
                                 assistant_messages.append({ "role": msg.role, "content": msg.content })
                     logger.info(f"Assistant messages: {assistant_messages}")
-                    return assistant_messages
+                    return jsonify({"result":assistant_messages}), 200
                 time.sleep(5)
     except Exception as e:
         logger.error(f"Error in run_openai: {str(e)}")
